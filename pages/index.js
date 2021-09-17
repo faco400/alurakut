7b208dcdbbe9edd915d43481b279694ea82a6e18
faco400 <vd.souza1999@gmail.com>
2021-09-17 16:37:01 -0300

    adding test on pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
 import MainGrid from '../src/components/MainGrid/MainGrid'
 import Box from '../src/components/Box/Box'
-import {AlurakutMenu, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons'
+import {AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet} from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSideBar(propriedades){
   return(
     <Box>
       <img src = {`http://github.com/${propriedades.profile}.png`}/>
+      <hr/>
+
+      <div>
+        <a className="boxLink" href={`http://github.com/${propriedades.profile}`}>
+          @{propriedades.profile}
+        </a>
+      </div>
+
+      <hr/>
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
@@ -22,7 +33,8 @@ export default function Home() {
     'pedro-cella',
     'gabrielapivetta',
     'jschneiderm98',
-    'saracampss'
+    'saracampss',
+    'eddie-gomes'
   ]
 
   return (
@@ -39,19 +51,39 @@ export default function Home() {
             Bem-Vindo(a)
             <OrkutNostalgicIconSet/>
           </Box>
+
+          <Box>
+            <h2 className="subTitle">Crie sua comunidade</h2>
+            <form onSubmit={function handleCreateCommunity(){
+              alert("oi");
+            }}>
+              <input
+                placeholder="Insira o nome da sua comunidade"
+                type="text" 
+                aria-label="Insira o nome da sua comunidade"
+                name="title"
+              />
+              <input
+                placeholder="Insira URL para foto de capa"
+                aria-label="Insira URL para foto de capa"
+                name="image"
+              />
+              <button>Enviar</button>
+            </form>
+          </Box>
         </div>
 
         <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
           <ProfileRelationsBoxWrapper> 
-            <h2 className = "smalltitle" >
+            <h2 className = "smallTitle" >
               Pessoas Favoritas ({pessoasFavoritas.length})
             </h2>
 
             <ul>
               {pessoasFavoritas.map((ItemAtual) => {
                 return(
-                  <li>
-                    <a href = {`http://github.com/${ItemAtual}`} key={ItemAtual}>
+                  <li key={ItemAtual}>
+                    <a href = {`http://github.com/${ItemAtual}`}>
                       <img src ={`http://github.com/${ItemAtual}.png`}/>
                       <span>{ItemAtual}</span>
                     </a>
